dcecfcbc6e52be312cd2132955a68510a7bf964b
pankajparis <158778529+pankajparis@users.noreply.github.com>
2025-01-23 10:50:11 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import DreamTeller from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DreamTeller />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
