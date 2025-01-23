1b96d5d7f18f4e245e70c035dd956bc601a3c0f1
pankajparis <158778529+pankajparis@users.noreply.github.com>
2025-01-23 17:00:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Ensure you have an App.js file in the src directory
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
